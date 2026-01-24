6614a418771acea61deb89d9a4f907fab1649533
unknown <pantho883@gmail.com>
2026-01-25 02:35:08 +0600

    minor bug fix on sendmail and userSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -15,19 +15,19 @@ const userSchema = new mongoose.Schema({
 },{timestamps:true})
  
 
-userSchema.pre('save',async function (next) {
+userSchema.pre('save',async function () {
 
-   if(!this.isModified('password')) return next()
+   if(!this.isModified('password')) return 
 
       try{
          const salt = await bcrypt.genSalt(10)
          const hashedPassword = await bcrypt.hash(this.password,salt)
          this.password = hashedPassword
-         next()
+     
 
       }catch(err){
          console.log(err)
-         next()
+        
       }
    
 })
--- utilities/sendMail.js
+++ utilities/sendMail.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 // Create a transporter using Ethereal test credentials.
 // For production, replace with your actual SMTP server details.
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false, // Use true for port 465, false for port 587
   auth: {
